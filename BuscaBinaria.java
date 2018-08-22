900d29b537e4d45c5a39ddf72f4002717acb700e
IsmaelRDC <42240015+IsmaelRDC@users.noreply.github.com>
2018-08-22 09:45:53 -0300

    Update BuscaBinaria.java

--- BuscaBinaria.java
+++ BuscaBinaria.java
@@ -1,44 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bubllesort;
+
+package ativJava;
 import  java.util.Scanner;
-/**
- *
- * @author Roberta
- */
-public class BuscaBinaria {
+
+public class Buscabinaria {
     public static void main(String[] args) {
-        Scanner tck = new Scanner(System.in);
-        System.out.println("Digite quantas posições o vetor deverá ter: ");
-        int tam = tck.nextInt();
-        int vetor[] = new int[tam];
+        Scanner tcl = new Scanner(System.in);
+        System.out.println("Digite as posições do vetor: ");
+        int tamanho = tcl.nextInt();
+        int vetor[] = new int[tamanho];
         for (int i = 0; i <vetor.length; i++) {
             vetor[i] = i;
         }
-    int minim = vetor[0];
-    int maxim = vetor.length;
-    int medio = (minim+maxim/2);
-        System.out.println("Digite o valor que deseja encontrar:");
-    int valor = tck.nextInt();
-        if (valor < medio) {
+    int minimo = vetor[0];
+    int maximo = vetor.length;
+    int media = (minimo+maximo)/2;
+        System.out.println("Digite o valor do vetor:");
+    int valor = tcl.nextInt();
+        if (valor < media) {
             for (int i = 0; i <vetor.length; i++) {
                 if (valor == vetor[i]) {
-                    System.out.println("o vetor se encotra na posição"+ i);
+                    System.out.println("O vetor está na posição :  "+ i);
                 }
             }
         }
-        else if (valor > medio){
+        else if (valor > media){
             for (int i = 0; i < 10; i++) {
                 if (valor == vetor[i]) {
-                    System.out.println("o vetor se encontra na posição"+ i);
+                    System.out.println("O vetor está na posição :"+ i);
                 }
             } 
         }
         else{
-            System.out.println("O vetor se encontra na posição"+ medio);
+            System.out.println("O vetor está na posição :"+ media);
         }
     }
 }
